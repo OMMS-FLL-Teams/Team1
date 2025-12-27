6b6c6ebe38beed906fae55c72f290dc97d617736
OMMS-FLL-Teams <omms-fll-coaches@outlook.com>
2025-12-27 15:01:25 -0500

    Update SM_t6_rev7.py

--- Submerged/SM_t6_rev7.py
+++ Submerged/SM_t6_rev7.py
@@ -1,5 +1,8 @@
 #  Task 6 - M15 Research Vessel - Part 1 - drop off samples into vessel
 #
+# This code is released unde the Apache License v2.0
+# https://www.apache.org/licenses/LICENSE-2.0.txt
+#
 #
 # task revision number
 t6_revision="6"
