4e345e46c421143de79ce4f0a55ac0244549d14f
OMMS-FLL-Teams <omms-fll-coaches@outlook.com>
2025-12-27 14:58:06 -0500

    Update tN_revN_starter_code.py

--- Robot_Library/tN_revN_starter_code.py
+++ Robot_Library/tN_revN_starter_code.py
@@ -2,6 +2,8 @@
 #
 #  This code is intended to demonstration the how to use the robot library.
 #
+# This code is released unde the Apache License v2.0
+# https://www.apache.org/licenses/LICENSE-2.0.txt
 #
 # task revision number
 task_number="1"     # enter the task number here 
