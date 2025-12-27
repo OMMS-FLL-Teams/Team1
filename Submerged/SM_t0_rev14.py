977150a8aa7a3c6ced61e6513eebf989e215f3f5
OMMS-FLL-Teams <omms-fll-coaches@outlook.com>
2025-12-27 14:59:23 -0500

    Update SM_t0_rev14.py

--- Submerged/SM_t0_rev14.py
+++ Submerged/SM_t0_rev14.py
@@ -1,5 +1,8 @@
 #  Task 0 - Cleanup Mat and deliver to right home
 #
+# This code is released unde the Apache License v2.0
+# https://www.apache.org/licenses/LICENSE-2.0.txt
+#
 # task revision number
 t0_revision="14"
 #
