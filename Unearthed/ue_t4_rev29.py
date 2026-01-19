ce2481e65fceb45881b55d8439dfbc7198d18ba3
OMMS-FLL-Teams <omms-fll-coaches@outlook.com>
2026-01-19 17:21:29 -0500

    Add files via upload
    team's nearly final code

---
+++ Unearthed/ue_t4_rev29.py
@@ -0,0 +1,53 @@
+# Robot Lib Demonstration
+#
+# task revision number
+task_number="4"     # enter the task number here 
+task_revision="29"
+#
+import robot_library_rev33 as RL
+
+# change # in T#_Run() to the number of the task
+async def T4_Run(): 
+
+    #   enter the list of missions
+    print ("task# ",task_number," task_rev ",task_revision," Missions 5/Who Lived Here, 6/The Forge,  9/Whats on Sale (red paddle), 10/Tip the Scale (bucket), ")
+    await RL.initializeRobotForTask()
+    
+    waitForPrompt=False
+    RL.debugL1=False
+    RL.debugL2=False
+
+    await RL.wait(500)
+    if RL.debugL1: print(" the forge ")
+    await RL.driveRobot(525,800,500,"hold",waitForPrompt)
+    await RL.moveAttachment("right",-162,500,False,"coast",False,True, waitForPrompt)
+    await RL.turnRobot(22,300,400,"hold",waitForPrompt)
+
+    if RL.debugL1: print(" who lived here ")
+    await RL.turnRobot(-22,300,400,"hold",waitForPrompt)
+    await RL.driveRobot(45,800,500,"hold",waitForPrompt)
+    await RL.moveAttachment("right",-146,300,False,"hold",False,True, waitForPrompt)
+    await RL.turnRobot(-27,300,400,"hold",waitForPrompt)
+    await RL.turnRobot(10,300,400,"hold",waitForPrompt)
+    await RL.moveAttachment("right",-1,500,False,"hold",False,True, waitForPrompt)
+    await RL.driveRobot(20,800,500,"hold",waitForPrompt)
+
+    if RL.debugL1: print("  what's on sale - red paddle")
+    await RL.turnRobot(-46,300,400,"hold",waitForPrompt)
+    await RL.driveRobot(430,600,500,"hold",waitForPrompt)
+    await RL.turnRobot(15,300,400,"hold",waitForPrompt)
+    await RL.driveRobot(-215,600,500,"hold",waitForPrompt)
+
+    
+    if RL.debugL1: print("   tip the scales - bucket")        
+    await RL.driveRobot(70,800,600,"hold",waitForPrompt)
+    await RL.turnRobot(-110,400,400,"hold",waitForPrompt)
+    await RL.moveAttachment("right",-150,300,False,"hold",False,True, waitForPrompt)
+    await RL.moveAttachment("right",0,300,False,"hold",False,True, waitForPrompt)
+    await RL.turnRobot(-80,400,400,"hold",waitForPrompt)
+    await RL.driveRobot(350,800,600,"hold",waitForPrompt)
+    await RL.turnRobot(49,400,400,"hold",waitForPrompt)
+    await RL.driveRobot(570,800,600,"hold",waitForPrompt)
+    #await RL.driveRobotAndLift(0,800,600,"left",50,200,"hold",False,True, waitForPrompt)
+
+
